96160ed5a96459a46cb329ca209c910f8655ac87
AlvaroP1504 <cod.black.opps0@gmail.com>
2023-07-29 07:38:58 +0000

    Apollo Setup: 100%

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { createApp } from 'vue'
+import { createApp, h, provide } from 'vue'
 import './style.css'
 import router from './routes/routes'
 import App from './App.vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-createApp(App)
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://countries.trevorblades.com/',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const app = createApp({
+    setup() {
+        provide(DefaultApolloClient, apolloClient)
+    },
+    render: () => h(App)
+})
+
+app
 .use(router)
 .mount('#app')
+// createApp(App)
+// .use(router)
+// .mount('#app')
