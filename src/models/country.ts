a825e306f9dcf8d5a444b32120ab98a8456b9fc1
AlvaroP1504 <cod.black.opps0@gmail.com>
2023-07-29 10:30:01 +0000

    models created

---
+++ src/models/country.ts
@@ -0,0 +1,20 @@
+import type { Continent } from "./continent";
+interface BasicInfo{
+    code:string,
+    name:string
+}
+
+export interface CountryCard{
+    code:string,
+    name:string,
+    continent:Continent
+}
+export interface Country{
+    code:      string;
+    name:      string;
+    continent: Continent;
+    capital:   string;
+    currency:  string;
+    languages: BasicInfo[];
+    states:    BasicInfo[];
+}
